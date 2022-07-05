77e75ae7671a8eb45d226aea3bd2dcf4e8c67e70
rkeshmir@yahoo.com <aUtUmN2)1*>
2022-07-05 21:32:16 +0200

    Recipes overview page: test (Eight items are shown on each page, I can share the page URL and see the same result)

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Recipe } from "../../types";
 import { RecipeCard } from "../../components/recipe-card";
 
 const Recipes = () => {
   const pageSize = 8;
-  const { page } = useParams();
+  const location = useLocation();
   const navigate = useNavigate();
   const [cocktails, setCocktails] = useState<Recipe[]>([]);
   const [pageCount, setPageCount] = useState(1);
-  const isCurrentPage = (pageNumber: number) =>
-    pageNumber === parseInt(page || "0", 10);
+  const getPage = (pathname: string) => {
+    const pageStr = pathname.match(/\d+/);
+    if (!pageStr) return 0;
+    return parseInt(pageStr[0], 10);
+  };
+  const page = () => getPage(location.pathname);
+  const isCurrentPage = (pageNumber: number) => pageNumber === page();
   const shiftPage = (dir: 1 | -1) => {
-    const dest = parseInt(page || "0", 10) + dir;
+    const dest = page() + dir;
     if (dest < 1 || dest > pageCount) return;
     navigate(`/recipes/${dest}`);
   };
@@ -21,7 +26,7 @@ const Recipes = () => {
       const response = await fetch(
         `/api/recipes/all?${new URLSearchParams({
           limit: pageSize.toString(),
-          offset: ((parseInt(page || "0", 10) - 1) * 8).toString(),
+          offset: ((getPage(location.pathname) - 1) * 8).toString(),
         })}`
       );
       return response.json();
@@ -30,13 +35,13 @@ const Recipes = () => {
       setCocktails(res.data);
       setPageCount(Math.ceil(res.total / pageSize));
     });
-  }, [page]);
+  }, [location]);
   return (
     <main className="flex h-full flex-col">
       <nav
         className="mb-2 flex justify-center"
-        aria-label="Page navigation"
-        data-testid="page-navigation"
+        role="navigation"
+        aria-label="Pagination Navigation"
       >
         <ul className="inline-flex -space-x-px">
           <li>
@@ -44,6 +49,8 @@ const Recipes = () => {
               onClick={() => shiftPage(-1)}
               type="button"
               data-testid="previous-page"
+              aria-disabled={page() < 2 ? "true" : "false"}
+              aria-label={`Goto Page ${page() - 1}`}
               className="ml-0 w-24 rounded-l-lg border border-gray-300 bg-white py-2 px-3 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Previous
@@ -61,13 +68,18 @@ const Recipes = () => {
               >
                 <button
                   type="button"
-                  aria-current={isCurrentPage(pageNumber) ? "page" : undefined}
                   className={`border border-gray-300 py-2 px-3 dark:border-gray-700${
                     isCurrentPage(pageNumber)
                       ? " bg-blue-50 text-blue-600 hover:bg-blue-100 hover:text-blue-700 dark:bg-gray-700 dark:text-white"
                       : " bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                   }`}
                   onClick={() => navigate(`/recipes/${pageNumber}`)}
+                  aria-label={
+                    isCurrentPage(pageNumber)
+                      ? `Current Page, Page ${pageNumber}`
+                      : `Goto Page ${pageNumber}`
+                  }
+                  aria-current={isCurrentPage(pageNumber) ? "true" : undefined}
                 >
                   {pageNumber}
                 </button>
@@ -78,7 +90,7 @@ const Recipes = () => {
             <input
               data-testid="page-navigation-input"
               className="h-full border border-gray-300 py-2 px-3"
-              value={page}
+              value={page()}
               min={0}
               max={pageCount}
               type="number"
@@ -95,6 +107,8 @@ const Recipes = () => {
               onClick={() => shiftPage(1)}
               type="button"
               data-testid="next-page"
+              aria-disabled={page() >= pageCount ? "true" : "false"}
+              aria-label={`Goto Page ${page() - 1}`}
               className="w-24 rounded-r-lg border border-gray-300 bg-white py-2 px-3 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               Next
--- src/pages/recipes/recipes.test.js
+++ src/pages/recipes/recipes.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import Recipes from "./index";
 
-import Homepage from "./index";
+it("renders navigation and eight recipes", async () => {
+  render(
+    <MemoryRouter initialEntries={["/recipes/1"]}>
+      <Recipes />
+    </MemoryRouter>
+  );
+  expect(screen.getByRole("navigation")).toBeInTheDocument();
+  expect(screen.getByLabelText("Current Page, Page 1")).toBeInTheDocument();
+  expect(await screen.findAllByTestId(/recipe-/i)).toHaveLength(8);
+});
 
-it("renders api message", () => {
-  render(<Homepage />);
-  expect(screen.getByText("/api/recipes/all")).toBeInTheDocument();
+it("navigates on user click on another page", async () => {
+  const user = userEvent.setup();
+  render(
+    <MemoryRouter initialEntries={["/recipes/1"]}>
+      <Recipes />
+    </MemoryRouter>
+  );
+  await user.click(await screen.findByLabelText("Goto Page 2"));
+  expect(
+    await screen.findByLabelText("Current Page, Page 2")
+  ).toBeInTheDocument();
+  expect(await screen.findAllByTestId(/recipe-/i)).toHaveLength(8);
 });
