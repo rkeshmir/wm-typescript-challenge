ee3f1cc16df433794d6df7c58518186a3f45295e
rkeshmir@yahoo.com <aUtUmN2)1*>
2022-07-06 15:41:29 +0200

    Responsive navigation: test routing

--- src/__tests__/router.test.js
+++ src/__tests__/router.test.js
@@ -4,9 +4,24 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import { Router } from "../router";
+import NotFound from "../pages/not-found";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 const renderWithRouter = (ui, { route = "/" } = {}) => {
-  window.history.pushState({}, "Test page", route);
+  window.history.pushState({}, "Test page", route); //  TODO: the passed route is not applied correctly
 
   return {
     user: userEvent.setup(),
@@ -15,15 +30,24 @@ const renderWithRouter = (ui, { route = "/" } = {}) => {
 };
 
 test("full app rendering/navigating", async () => {
-  const { user } = await renderWithRouter(
+  const { user } = renderWithRouter(
     <Suspense fallback={null}>
       <Router />
-    </Suspense>
+    </Suspense>,
+    { route: "" }
+  );
+  expect(await screen.findAllByRole("navigation")).toHaveLength(2);
+  await user.click(await screen.findByLabelText("Goto Page Recipes"));
+  await user.click(await screen.findByLabelText("Goto Page Home"));
+});
+
+test("Show not found page", async () => {
+  const badRoute = "/some-bad-route";
+  render(
+    <MemoryRouter initialEntries={[badRoute]}>
+      <NotFound />
+    </MemoryRouter>
   );
 
-  // Check on the recipes link in the navbar
-  await user.click(await screen.findByTestId(/navbar-link--recipes/));
-
-  // Check if the recipes api tekst is visible
-  expect(screen.getByText("/api/recipes/all")).toBeInTheDocument();
+  expect(await screen.findByRole("alert")).toBeInTheDocument();
 });
